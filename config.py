fe2bfe8e0f4f0a67b911952fb9195200fce9bcee
Ivan Villa <itanalytics21@gmail.com>
2022-11-28 20:33:57 -0800

    Combined API file

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 api_key = "19fb554c7f359f01d05046836d05ea21"
 
 #Spotify Client ID
-spotify_id = 'b4d023fcb19148bb998d675f89b6c302'
+CLIENT_ID = 'b4d023fcb19148bb998d675f89b6c302'
+CLIENT_SECRET = '81930c900ac44ddcbbf9517abc26f15b'
 
 #Napster API Key
 napster_key = 'ZmMxYTQ3NzYtZjU0ZS00YTM2LTliMDQtNzAxN2JjNzY1ZDAw'
